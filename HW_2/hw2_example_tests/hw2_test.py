b1e1cef16e16155a07bd9ea82d07a8b34d686e93
Kfir-Barzilay <129737082+Kfir-Barzilay@users.noreply.github.com>
2024-07-20 10:40:38 +0300

    Add files via upload

--- HW_2/hw2_example_tests/hw2_test.py
+++ HW_2/hw2_example_tests/hw2_test.py
@@ -30,6 +30,7 @@ def testErrno() :
         poll_ret = pyMpi.poll([ppid], -1)
     except Exception, ex:
         assert (ex.errno == errno.EINVAL)
+    print("**********************End of test Errno*************************")
 
     
 
@@ -84,7 +85,7 @@ def test2():
     try:
         poll_ret = pyMpi.poll([cpid2], 5)
     except Exception, ex:
-        print("The exception is: " , ex)
+        assert (ex.errno == errno.ETIMEDOUT)
 
     e = time.time()
     elapsed = e - s
@@ -164,7 +165,7 @@ def test5():
     try:
         poll_ret = pyMpi.poll([cpid1], 5)
     except Exception, ex:
-        print("The exception is: " , ex)
+        print("The exception is: " , ex.errno)
     
     e = time.time()
     elapsed = e - s
@@ -175,25 +176,69 @@ def test5():
 
 def test6():
     print("**********************starting test 6******************************")
-    """ not looking for all msges """
+    """ Reciving fro,m dead proc and alive proc """
     ppid = os.getpid()
     pyMpi.register()
     cpid1 = os.fork()
+    cpid2 = os.fork()
     if cpid1 == 0:
         pyMpi.send(ppid, "Hello1!")
-        print("the msg was sent")
         os._exit(0)
-    _, status = os.waitpid(cpid1, 0)  # Wait for child1 to finish
+   
+    if cpid2 == 0:
+        pyMpi.send(ppid, "Hello2!")
+        time.sleep(7)
+        os._exit(0)
     
+    _, status = os.waitpid(cpid1, 0)  # Wait for child1 to finish
     time.sleep(1)
     try:
-        print('try receive msg')
-        pyMpi.poll([cpid1] , 1)
-        print (message)
+        poll_ret = pyMpi.poll([cpid1, cpid2] , 5)
     except Exception, ex:
-        print("The exception is: " , ex)
-    
+        print("The exception is: " , ex.errno)
+    assert (poll_ret[0] == cpid1)
+    assert (poll_ret[1] == cpid2)
+
+    _, status = os.waitpid(cpid2, 0)  # Wait for child1 to finish
     print("**********************end of test 6******************************")
+
+def test7():
+    print("**********************starting test 7******************************")
+    """ Reciving fro,m dead proc and alive proc """
+    ppid = os.getpid()
+    pyMpi.register()
+    cpid1 = os.fork()
+    cpid2 = os.fork()
+    if cpid1 == 0:
+        time.sleep(5)
+        pyMpi.send(ppid, "Hello1!")
+        os._exit(0)
+   
+    if cpid2 == 0:
+        time.sleep(1)
+        pyMpi.send(ppid, "Hello2!")
+        os._exit(0)
+    
+    try:
+        poll_ret = pyMpi.poll([cpid1, cpid2] , 2)
+    except Exception, ex:
+        print("The exception is: " , ex.errno)
+    assert (poll_ret[0] == cpid2)
+    pyMpi.receive(poll_ret[0] , 100)
+
+    try:
+        poll_ret = pyMpi.poll([cpid1, cpid2] , 3)
+    except Exception, ex:
+        assert (ex.errno == errno.ETIMEDOUT)
+    
+    try:
+        poll_ret = pyMpi.poll([cpid1, cpid2] , 3)
+    except Exception, ex:
+        print("The exception is: " , ex.errno)
+    assert (poll_ret[0] == cpid1)
+
+    _, status = os.waitpid(cpid2, 0)  # Wait for child1 to finish
+    print("**********************end of test 7******************************")
 
 if __name__ == "__main__":
     print("**********************tests**************************************")
@@ -204,3 +249,4 @@ if __name__ == "__main__":
     test4()
     test5()
     test6()
+    test7()
